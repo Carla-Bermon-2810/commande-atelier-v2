f9b416151a3c4d4ae0f5e59e2e5cae2ee10b627a
Carla-Bermon-2810 <bermon.carla.dl@gmail.com>
2026-09-25 08:09:56 +0200

    Définir l’icône du site avec le logo officiel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Commande Atelier | Découpe Laser",
   description: "Catalogue et commandes internes de l'atelier Découpe Laser.",
+  icons: { icon: "/d%C3%A9coupe%20laser%20logo%20PNG%202023.png" },
 };
 
 export default function RootLayout({
